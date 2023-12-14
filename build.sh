89c2bc85f8e0b1ff59974c53461ec4b8b50586e2
Joshua Attridge <joshyattridge@gmail.com>
2023-12-14 12:00:03 +0000

    using sdk qmake

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p build/dbus-modbustcp
 cd build/dbus-modbustcp
-qmake CXX=$CXX ../../dbus_modbustcp.pro && make
+/opt/venus/current/sysroots/x86_64-ve-linux/usr/bin/qmake CXX=$CXX ../../dbus_modbustcp.pro && /opt/venus/current/sysroots/x86_64-ve-linux/usr/bin/make
