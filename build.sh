6ad79685647844da3f6e11ecf9800142da8e5e5e
Joshua Attridge <joshyattridge@gmail.com>
2024-01-15 12:08:09 +0000

    added QMAKESPEC to build

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 mkdir -p build/dbus-modbustcp
 cd build/dbus-modbustcp
+export QMAKESPEC=/opt/venus/current/sysroots/x86_64-ve-linux/usr/mkspecs/linux-g++
 /opt/venus/current/sysroots/x86_64-ve-linux/usr/bin/qmake CXX=$CXX ../../dbus_modbustcp.pro && make
