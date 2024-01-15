99d6a84a11ab12ecb5cd616d373fdb3970e7f79e
Joshua Attridge <joshyattridge@gmail.com>
2024-01-15 16:17:16 +0000

    full build

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-. /opt/venus/current/environment-setup-cortexa8hf-neon-ve-linux-gnueabi
-
 mkdir -p build/dbus-modbustcp
 cd build/dbus-modbustcp
 qmake CXX=$CXX ../../dbus_modbustcp.pro && make
