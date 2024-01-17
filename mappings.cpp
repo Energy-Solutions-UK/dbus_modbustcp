d5d2069163247c359a27c7a119c96f2570bbd316
Joshua Attridge <joshyattridge@gmail.com>
2024-01-17 11:54:02 +0000

    test string

--- mappings.cpp
+++ mappings.cpp
@@ -337,8 +337,9 @@ void Mappings::setValues(MappingRequest *request)
 									  it.data()->scaleFactor);
 			break;
 		case mb_type_string:
-			dbusValue = convertToDbus(it.data()->dbusType, static_cast<QString>(value),
-									  it.data()->scaleFactor);
+			dbusValue = convertToDbus(it.data()->dbusType, value,
+									it.data()->scaleFactor);
+			break;
 		default:
 			// Do nothing. dbusValue will remain invalid, which will generate an error below.
 			break;
@@ -410,8 +411,6 @@ template<class rettype> rettype Mappings::convertFromDbus(const QVariant &value,
 		return static_cast<rettype>(round(value.toUInt() * scaleFactor));
 	case QMetaType::Bool:
 		return static_cast<rettype>(value.toBool());
-	case QMetaType::QString:
-		return static_cast<rettype>(value.toString());
 	default:
 		QLOG_WARN() << "[Mappings] convert from dbus type tries to convert an unsupported type:"
 					<< value.type() << "(" << value.typeName() << ")";
@@ -422,7 +421,6 @@ template<class rettype> rettype Mappings::convertFromDbus(const QVariant &value,
 template<class argtype> QVariant Mappings::convertToDbus(QMetaType::Type dbusType,
 														 argtype value, double scaleFactor)
 {
-	QLOG_DEBUG() << "Convert to string" << value;
 	switch (dbusType) {
 	case QMetaType::Float:
 	case QMetaType::Double:
@@ -442,7 +440,7 @@ template<class argtype> QVariant Mappings::convertToDbus(QMetaType::Type dbusTyp
 	case QMetaType::Bool:
 		return QVariant::fromValue(static_cast<int>(value));
 	case QMetaType::QString:
-		return QVariant::fromValue(static_cast<QString>(value));
+		return QVariant::fromValue(QString::number(value));
 	default:
 		QLOG_WARN() << "[Mappings] convert to dbus type tries to convert an unsupported type:"
 					<< dbusType;
