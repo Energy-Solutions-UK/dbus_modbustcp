5433a2f0e2023833165b1800c20033b413afd75e
Joshua Attridge <joshyattridge@gmail.com>
2024-01-18 16:07:42 +0000

    test

--- mappings.cpp
+++ mappings.cpp
@@ -358,7 +358,7 @@ void Mappings::setValues(MappingRequest *request)
 			{
 			QLOG_ERROR() << "here3";
 			QLOG_ERROR() << data;
-			dbusValue = convertToDbus(it.data()->dbusType, QString(data), it.data()->scaleFactor);
+			dbusValue = convertToDbus<QString>(it.data()->dbusType, QString(data), it.data()->scaleFactor);
 			}
 		default:
 			// Do nothing. dbusValue will remain invalid, which will generate an error below.
