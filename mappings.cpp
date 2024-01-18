178a91d8ac503b484541bd25405a14c16fdb0127
Joshua Attridge <joshyattridge@gmail.com>
2024-01-18 16:20:02 +0000

    added to .h

--- mappings.cpp
+++ mappings.cpp
@@ -438,7 +438,7 @@ template<class rettype> rettype Mappings::convertFromDbus(const QVariant &value,
 	}
 }
 
-template<class argtype> QVariant Mappings::convertToDbus<QString>(QMetaType::Type dbusType, QString value, double scaleFactor)
+QVariant Mappings::convertToDbus(QMetaType::Type dbusType, QString value, double scaleFactor)
 {
     switch (dbusType) {
     case QMetaType::QString:
