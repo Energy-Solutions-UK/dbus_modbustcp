828012898cca697d3e19e6e852b10af35257b21d
Joshua Attridge <joshyattridge@gmail.com>
2024-01-19 09:07:44 +0000

    removed cant write to strings message

--- mappings.cpp
+++ mappings.cpp
@@ -684,11 +684,6 @@ Mappings::DBusModbusData::DBusModbusData(QString _deviceType, QStringList _objec
 		_scaleFactor = 1;
 	scaleFactor = _scaleFactor;
 
-	// if (_modbusType == mb_type_string && _accessRights == mb_perm_write) {
-	// 	_accessRights = mb_perm_read;
-	// 	QLOG_WARN() << "[Mappings] Register" << _deviceType << _objectPaths[0]
-	// 				<< ": cannot write string values";
-	// }
 	accessRights = _accessRights;
 }
 
